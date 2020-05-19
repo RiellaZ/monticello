eb0ff924547cfeb26cacc8ad10d7cc89ce0d0f29
Gabriella <gabriellazamudio@yahoo.se>
2020-05-19 11:31:42 +0200

    smooth scroll

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+$('#bold').on('click', () => {
+    let circle = $('.fa-circle');
+    circle.css('font-weight', 'bold');
+});
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+         $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
