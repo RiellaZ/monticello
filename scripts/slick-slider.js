d1d71152f986e55686574087554aaa447f5abbdc
Gabriella <gabriellazamudio@yahoo.se>
2020-05-20 14:43:51 +0200

    map, slider and general clean up

--- scripts/slick-slider.js
+++ scripts/slick-slider.js
@@ -1,6 +1,12 @@
-/*$(document).ready(function() {
-    $('.slick__content').slick();
-});*/
-
-
 $('.slick__container').slick();
+
+//I have tried every solution I can think of but it just won't line up.
+
+$('.news__slickslider').slick({
+    infinite: true,
+    centerMode: true, 
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+
+  //Same here, I don't understand why it doesn't work.
